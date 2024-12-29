107224309a29873c2ccb1143c37306a520dbb546
holbroow <willholbrook10@gmail.com>
2024-12-29 01:26:44 +0000

    Upload marginal progress on raylib implementation

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+CC := C:/raylib/w64devkit/bin/gcc.exe
+
 all:
-	gcc -o test src/Main.c src/holbroow6502.c src/Bus.c src/PPU.c src/Cartridge.c -O3
+	$(CC) -IC:/raylib/raylib/src -LC:/raylib/raylib/src -o test src/Main.c src/holbroow6502.c src/Bus.c src/PPU.c src/Cartridge.c -lraylib
--- src/Main.c
+++ src/Main.c
@@ -4,58 +4,63 @@
 
 #define SDL_MAIN_HANDLED
 
-#include "Bus.h"
-#include "holbroow6502.h"
-#include "PPU.h"
-#include "Cartridge.h"
+#define WIN32_LEAN_AND_MEAN
+#define NOGDI
+
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <windows.h>
+#include <C:/raylib/raylib/src/raylib.h>
+// #include <windows.h>
+
+#include "Bus.h"
+#include "holbroow6502.h"
+#include "PPU.h"
+#include "Cartridge.h"
 
 // Define display parameters
 #define NES_WIDTH 256
 #define NES_HEIGHT 240
 #define SCALE 2  // Scale factor for visibility
 
-// Helper function to get current time in nanoseconds
-uint64_t get_time_ns() {
-    // ASSUMING WINDOWS (NTS: WOULD LIKE TO ADD COMPILER SUPPORT FOR LINUX LATER)
-    LARGE_INTEGER frequency;
-    LARGE_INTEGER counter;
-    if (!QueryPerformanceFrequency(&frequency)) {
-        fprintf(stderr, "[CPU] Time, QueryPerformanceFrequency failed!\n");
-        return 0;
-    }
-    if (!QueryPerformanceCounter(&counter)) {
-        fprintf(stderr, "[CPU] Time, QueryPerformanceCounter failed!\n");
-        return 0;
-    }
-    return (uint64_t)((counter.QuadPart * 1000000000LL) / frequency.QuadPart);
-}
+// // Helper function to get current time in nanoseconds
+// uint64_t get_time_ns() {
+//     // ASSUMING WINDOWS (NTS: WOULD LIKE TO ADD COMPILER SUPPORT FOR LINUX LATER)
+//     LARGE_INTEGER frequency;
+//     LARGE_INTEGER counter;
+//     if (!QueryPerformanceFrequency(&frequency)) {
+//         fprintf(stderr, "[CPU] Time, QueryPerformanceFrequency failed!\n");
+//         return 0;
+//     }
+//     if (!QueryPerformanceCounter(&counter)) {
+//         fprintf(stderr, "[CPU] Time, QueryPerformanceCounter failed!\n");
+//         return 0;
+//     }
+//     return (uint64_t)((counter.QuadPart * 1000000000LL) / frequency.QuadPart);
+// }
 
-// Cross-platform sleep function in microseconds
-void sleep_microseconds(uint64_t microseconds) {
-    // AGAIN, ASSUMING WINDOWS (NTS: WOULD LIKE TO ADD COMPILER SUPPORT FOR LINUX LATER)
-    if (microseconds >= 1000) {
-        // Sleep for the integer part of milliseconds
-        DWORD millis = (DWORD)(microseconds / 1000);
-        Sleep(millis);
-    }
+// // Cross-platform sleep function in microseconds
+// void sleep_microseconds(uint64_t microseconds) {
+//     // AGAIN, ASSUMING WINDOWS (NTS: WOULD LIKE TO ADD COMPILER SUPPORT FOR LINUX LATER)
+//     if (microseconds >= 1000) {
+//         // Sleep for the integer part of milliseconds
+//         DWORD millis = (DWORD)(microseconds / 1000);
+//         Sleep(millis);
+//     }
 
-    // Busy-wait for the remaining microseconds
-    LARGE_INTEGER frequency;
-    LARGE_INTEGER start, current;
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&start);
-    double wait_time = (double)(microseconds % 1000) / 1000000.0; // Convert to seconds
+//     // Busy-wait for the remaining microseconds
+//     LARGE_INTEGER frequency;
+//     LARGE_INTEGER start, current;
+//     QueryPerformanceFrequency(&frequency);
+//     QueryPerformanceCounter(&start);
+//     double wait_time = (double)(microseconds % 1000) / 1000000.0; // Convert to seconds
 
-    do {
-        QueryPerformanceCounter(&current);
-    } while (((double)(current.QuadPart - start.QuadPart) / frequency.QuadPart) < wait_time);
-}
+//     do {
+//         QueryPerformanceCounter(&current);
+//     } while (((double)(current.QuadPart - start.QuadPart) / frequency.QuadPart) < wait_time);
+// }
 
 // Load the program to main bus memory
 int load_program(Bus* bus, uint16_t start_address, const char* hex_string) {
@@ -138,6 +143,26 @@ int main(int argc, char* argv[]) {
     Cpu* cpu = init_cpu(bus);
     printf("[MANAGER] Initialising CPU finished!\n");
 
+
+    // Initialise Display
+    InitWindow(NES_WIDTH, NES_HEIGHT, "holbroowNes test");
+
+    // Create an Image from the framebuffer
+    Image framebufferImage = {
+        .data = ppu->framebuffer,
+        .width = NES_WIDTH,
+        .height = NES_HEIGHT,
+        .mipmaps = 1,
+        .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8
+    };
+
+    // Load the texture from the image
+    Texture2D framebufferTexture = LoadTextureFromImage(framebufferImage);
+
+    // Unload the image as it's no longer needed
+    UnloadImage(framebufferImage);
+
+
     // Set program counter to the reset vector (0xFFFC-0xFFFD)
     uint16_t reset_low = bus_read(bus, 0xFFFC);
     uint16_t reset_high = bus_read(bus, 0xFFFD);
@@ -159,7 +184,6 @@ int main(int argc, char* argv[]) {
     printf("\n");
     
 
-
     bool run_debug = false;
     bool run_capped = false;
     int capped_value = 50;
@@ -179,7 +203,7 @@ int main(int argc, char* argv[]) {
 
 
     // Initialize frame timing
-    uint64_t frame_start_time_ns = get_time_ns();
+    //uint64_t frame_start_time_ns = get_time_ns();
     cpu->cycles_left = CYCLES_PER_FRAME;
     
     while (cpu->running) {
@@ -213,33 +237,51 @@ int main(int argc, char* argv[]) {
             }
             
         } else {
-            // Update Display (need to add)
-            
+            // Update Display (need to complete)
+            BeginDrawing();
+
+            // Clear the screen (optional)
+            ClearBackground(BLACK);
+
+            // Update the texture with the framebuffer data
+            UpdateTexture(framebufferTexture, ppu->framebuffer);
+
+            // // Define source and destination rectangles
+            // Rectangle sourceRec = { 0.0f, 0.0f, (float)NES_WIDTH, (float)NES_HEIGHT };
+            // Rectangle destRec = { 0.0f, 0.0f, (float)NES_WIDTH, (float)NES_HEIGHT };
+            // Vector2 origin = { 0.0f, 0.0f };
+            // float rotation = 0.0f;
+
+            // // Draw the texture scaled up
+            // DrawTexturePro(framebufferTexture, sourceRec, destRec, origin, rotation, WHITE);
+            DrawTexture(framebufferTexture, 0, 0, WHITE);
+
+            EndDrawing();
 
             // Frame handling
             frame_num++;
 
             // Calculate how long the frame took
-            uint64_t frame_end_time_ns = get_time_ns();
-            uint64_t elapsed_time_ns = frame_end_time_ns - frame_start_time_ns;
-            int64_t sleep_time_ns = FRAME_TIME_PERSEC - elapsed_time_ns;
+            // uint64_t frame_end_time_ns = get_time_ns();
+            // uint64_t elapsed_time_ns = frame_end_time_ns - frame_start_time_ns;
+            // int64_t sleep_time_ns = FRAME_TIME_PERSEC - elapsed_time_ns;
 
-            // Sleep for the remaining time to maintain 60fps
-            if (sleep_time_ns > 0) {
-                //if ((frame_num % 60) == 0) {
-                    printf("[CPU] FRAME %d | Slept for %lld microseconds to retain clock speed.\n", frame_num, sleep_time_ns / 1000);
-                //}
-                sleep_microseconds((sleep_time_ns / 1000) - 1750);  // For some reason my timing is broken, and sleeps for slightly too long, this is jank... :0/
-                // printf("%d\n", (sleep_time_ns / 1000));  // debug for time slept
-            } else {
-                // Frame took longer than desired
-                //if ((frame_num % 60) == 0) {
-                    printf("[CPU] FRAME %d | Warning, Frame is lagging by %lld ns.\n", frame_num, -sleep_time_ns);
-                //}
-            }
+            // // Sleep for the remaining time to maintain 60fps
+            // if (sleep_time_ns > 0) {
+            //     //if ((frame_num % 60) == 0) {
+            //         printf("[CPU] FRAME %d | Slept for %lld microseconds to retain clock speed.\n", frame_num, sleep_time_ns / 1000);
+            //     //}
+            //     sleep_microseconds((sleep_time_ns / 1000) - 1750);  // For some reason my timing is broken, and sleeps for slightly too long, this is jank... :0/
+            //     // printf("%d\n", (sleep_time_ns / 1000));  // debug for time slept
+            // } else {
+            //     // Frame took longer than desired
+            //     //if ((frame_num % 60) == 0) {
+            //         printf("[CPU] FRAME %d | Warning, Frame is lagging by %lld ns.\n", frame_num, -sleep_time_ns);
+            //     //}
+            // }
 
-            // Start timer for the next frame
-            frame_start_time_ns = get_time_ns();
+            // // Start timer for the next frame
+            // frame_start_time_ns = get_time_ns();
 
             // Reset cycles_left to cycles per frame
             cpu->cycles_left = CYCLES_PER_FRAME;
